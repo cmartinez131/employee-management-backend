42097083c2422088f48faf78b298efe63e1a5c4a
cmartinez131 <75385410+cmartinez131@users.noreply.github.com>
2023-04-30 21:35:17 -0400

    initial commit wit h starter code

---
+++ database/models/index.js
@@ -0,0 +1,14 @@
+// register models, set up associations between tables, and generate barrel file for the models;
+
+const Course  = require('./Course');
+const Instructor  = require('./Instructor');
+
+Course.belongsTo(Instructor);
+Instructor.hasMany(Course);
+
+
+
+module.exports = {
+  Course,
+  Instructor
+};
