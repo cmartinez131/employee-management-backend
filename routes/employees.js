f833bd6cd7e67a939ce3c1f8fb9405e3055ec55f
cmartinez131 <75385410+cmartinez131@users.noreply.github.com>
2023-05-03 20:20:47 -0400

    fix typo in add employee route

--- routes/employees.js
+++ routes/employees.js
@@ -32,7 +32,7 @@ router.delete('/:id', ash(async(req, res) => {
 
 // Add new employee
 router.post('/', ash(async(req, res) => {
-  let newEmlpoyee = await Employee.create(req.body);
+  let newEmployee = await Employee.create(req.body);
   res.status(200).json(newEmployee);
 }));
 
